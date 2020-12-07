7ba7a242dd9415950867b7e960b89bc98665618c
Jefferson Amori da Rocha <jefferson_adr@hotmail.com>
2020-12-07 05:45:41 -0300

    Added bottom navigation menu

--- PlaystationWishlistWebSite/wwwroot/js/site.js
+++ PlaystationWishlistWebSite/wwwroot/js/site.js
@@ -2,38 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#loader-wrapper").show();
 
-function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-}
-function onFailure(error) {
-    console.log(error);
-}
-function renderButton() {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
-    });
+$(document).ready(function () {
+    if (navigator.share) {
+        $("#navButtonShare").find("a").click(function () {
+            if (navigator.share) {
+                navigator.share({
+                    title: "Playstation Wishlist",
+                    url: window.location.origin
+                }).then(() => {
+                    console.log("Thanks for sharing!");
+                })
+                    .catch(console.error);
+            } else {
+                window.alert("Functionality not supported by this browser.");
+            }
+        });
+    }
+
+    configurePageNavbar();
+    $("#loader-wrapper").hide();
+});
+
+function configurePageNavbar() {
+    if ($(window).width() <= 576) {
+        $("#topNavBar").hide();
+        $("#bottomNavBar").show();
+    } else {
+        $("#bottomNavBar").hide();
+        $("#topNavBar").show();
+    }
 }
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
-
-$("#loader-wrapper").hide();
+window.onresize = function () {
+    configurePageNavbar();
+};
