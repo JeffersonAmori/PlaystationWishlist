401340c286c6f7c33f6592e5a4c0677c944f53e1
jeffersonamori <jefferson_adr@hotmail.com>
2021-03-22 15:55:44 -0300

    Criação do projeto PlaystationWishlistApp

---
+++ PlaystationWishlistApp/PlaystationWishlistApp/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using MvvmHelpers;
+using System.Threading.Tasks;
+
+namespace PlaystationWishlistApp.ViewModels
+{
+    public class ViewModelBase : BaseViewModel
+    {
+        public virtual async Task Initialize() { }
+    }
+}
---
+++ PlaystationWishlistApp/PlaystationWishlistApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MvvmHelpers;
+using PlaystationWishlist.Core.Entities;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlaystationWishlistApp.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        private ObservableRangeCollection<PlaystationGame> gameList;
+
+        public HomeViewModel()
+        {
+            GameList = new ObservableRangeCollection<PlaystationGame>();
+        }
+
+        public ObservableRangeCollection<PlaystationGame> GameList { get => gameList; set => SetProperty(ref gameList, value); }
+
+        public override async Task Initialize()
+        {
+            HttpClient client = new HttpClient();
+            
+            var response = await client.GetAsync("https://playstationwishlistapi.azurewebsites.net/api/games"); 
+            var result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<PlaystationWishlist.Core.Entities.PlaystationGame>>(
+                await response.Content.ReadAsStringAsync());
+
+            GameList.AddRange(result);
+        
+            await Task.FromResult(true);
+        }
+    }
+}
